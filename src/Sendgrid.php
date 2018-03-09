138e9b8cc1e0e446132e0efd28a354eff7b0141c
Florent LOTTIN <flottin@gmail.com>
2018-03-09 10:18:58 +0100

    take off key

--- src/Sendgrid.php
+++ src/Sendgrid.php
@@ -3,7 +3,7 @@ namespace Tools;
 
 class Sendgrid{
 
-     public static function send($contentHtml)
+     public static function send($contentHtml, $apiKey)
      {
          $from = new \SendGrid\Email("Florent LOTTIN", "flottin@free.com");
          $subject = "Sending from php";
@@ -11,7 +11,7 @@ class Sendgrid{
 
          $content = new \SendGrid\Content("text/plain", 'contentHtml');
          $mail = new \SendGrid\Mail($from, $subject, $to, $content);
-         $apiKey = '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';
+         
          $sg = new \SendGrid($apiKey);
          $response = $sg->client->mail()->send()->post($mail);
 
